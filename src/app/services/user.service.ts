6ff2b8f202e24eb1bb88ae1c76e0ebc5e439e146
VishnuGettam <vishnugettam@gmail.com>
2021-03-26 11:16:22 +0530

    Update user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   restAPI: string = 'http://localhost:3000/users';
-  constructor( private _httpClient :HttpClient ) {
+  constructor(private _httpClient: HttpClient) {}
 
+  registerUser(user: any) {
+    return this._httpClient.post(this.restAPI, user);
+  }
 
-   }
+  getUsers() {
+    return this._httpClient.get<any[]>(this.restAPI);
+  }
 
-   registerUser(user:any){
-     return this._httpClient.post(this.restAPI,user);
-   }
+  getUserById(id: number): Observable<any> {
+    return this._httpClient.get<any>(this.restAPI + '/' + id);
+  }
 
-
+  getUsersByEmail(email: string) {
+    return this._httpClient.get<any[]>(this.restAPI + '?email=' + email);
+  }
 }
