34439ce9c08716852d92057af79a967f712a5f12
VishnuGettam <vishnugettam@gmail.com>
2021-04-06 00:27:06 +0530

    Complete Changes

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -4,13 +4,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.css'],
 })
 export class SettingsComponent implements OnInit {
   authStatus: any = JSON.parse(sessionStorage.getItem('authStatus') || '{}');
   user: any = {};
-  edit:boolean = false;
-  constructor(private _userService:UserService) { }
+  edit: boolean = false;
+  name: any;
+
+  constructor(private _userService: UserService) {}
 
   ngOnInit(): void {
     this._userService.getUserById(this.authStatus['id']).subscribe((user) => {
@@ -18,10 +20,14 @@ export class SettingsComponent implements OnInit {
     });
   }
 
-  EditProfile(id:number){
-      this.edit = true;
+  EditProfile(id: number) {
+    this.name = this.user.name;
+    this.edit = true;
   }
-  UpdateProfile(id:number){
+  UpdateProfile(user: any) {
+    user.name = this.name;
 
+    this._userService.UpdateUserbyId(user.id, user).subscribe();
+    this.edit = false;
   }
 }
